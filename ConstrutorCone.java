ad147e4ba6878d09d8d46104532e9821a0402c43
Angelo Araújo <angelo.aristides@ifc.edu.br>
2021-10-19 17:25:10 -0300

    Modelo construtor que faltava
    Construtor com três parâmetros raio, altura e tipo de tinta.

---
+++ ConstrutorCone.java
@@ -0,0 +1,16 @@
+package cubo;
+
+
+public class ConstrutorCone {
+
+Float raio;
+Float altura;
+int tinta;
+	
+
+	public ConstrutorCone (Float raio, Float altura, int tinta) {
+		this.raio= raio;
+		this.altura= altura;
+		this.tinta=tinta;
+	}
+	}
